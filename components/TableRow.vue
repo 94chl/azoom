f0bf4357097be8a66aa782f438d6478933215a8c
94chl <94chl@naver.com>
2024-09-21 01:52:57 +0900

    add: TableRow component

---
+++ components/TableRow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="root">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.root {
+  display: grid;
+  grid-template-columns: 1fr 1fr 10fr 2fr 2fr;
+  gap: 8px;
+  justify-content: center;
+  justify-items: center;
+  border-bottom: 1px solid #000;
+  padding: 8px;
+}
+</style>
