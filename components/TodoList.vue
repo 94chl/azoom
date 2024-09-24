eea5b3f76e9b8644dac09529d16bf11da016c535
94chl <94chl@naver.com>
2024-09-24 23:04:56 +0900

    add: list pagination

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,29 +1,62 @@
 <template>
-  <div id="todoList">
+  <div class="todoList">
     <Header></Header>
     <div>
       <TableHeader></TableHeader>
       <TodoListItem
-        v-for="value in todoListOrder"
+        v-for="value in todoListOrder.slice(
+          (page - 1) * Number(selectedNumOfList),
+          page * Number(selectedNumOfList)
+        )"
         :key="`todoList_${value}`"
         :list-item-id="value"
       ></TodoListItem>
+    </div>
+    <div class="listPages">
+      <UPagination
+        v-model="page"
+        :page-count="Number(selectedNumOfList)"
+        :total="todoListOrder.length"
+        show-last
+        show-first
+      />
+
+      <USelectMenu v-model="selectedNumOfList" :options="numOfList" />
     </div>
   </div>
 </template>
 
 <style>
-#todoList {
+.todoList {
   padding: 32px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 16px;
+}
+
+.listPages {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
 
 <script setup lang="ts">
 const todoStore = useTodoStore();
 const { todoListOrder } = storeToRefs(todoStore);
+const page = ref(1);
+const numOfList = ["3", "5", "10", "30", "50"];
+const selectedNumOfList = ref(numOfList[0]);
+
+watch(
+  () => selectedNumOfList.value,
+  () => {
+    page.value = 1;
+  },
+  { deep: true }
+);
 
 onMounted(() => {
   if (todoStore.todoListOrder.length < 1) todoStore.addTodoListDummy();
